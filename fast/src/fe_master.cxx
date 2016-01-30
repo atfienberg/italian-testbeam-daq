915da1fe05dfb3b9958b721346a3e824b722b3bc
UW Muon Lab <uw.muon.dropbox@gmail.com>
2016-01-29 18:14:05 -0800

    added base class for caen usb digitizers, got caen DT5720 running with the daq

--- fast/src/fe_master.cxx
+++ fast/src/fe_master.cxx
@@ -213,8 +213,18 @@ int SetupConfig() {
     workers.PushBack(new WorkerCaen1742(name, conf_dir + dev_conf_file));
   }
 
+  // Set up the caen5720 devices.
+  BOOST_FOREACH (const ptree::value_type &v,
+                 conf.get_child("devices.caen_5720")) {
+    string name(v.first);
+    string dev_conf_file(v.second.data());
+
+    workers.PushBack(new WorkerCaenDT5720(name, conf_dir + dev_conf_file));
+  }
+
   // Set up the DRS4 devices.
-  // BOOST_FOREACH (const ptree::value_type &v, conf.get_child("devices.drs4")) {
+  // BOOST_FOREACH (const ptree::value_type &v, conf.get_child("devices.drs4"))
+  // {
   //   string name(v.first);
   //   string dev_conf_file(v.second.data());
 
---
+++ fast/src/find_caens.cxx
@@ -0,0 +1,30 @@
+/**
+ * Find which USB caen digitizers are connected to the PC and what their id
+ * numbers are
+ */
+
+#include <iostream>
+#include "CAENDigitizer.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  CAEN_DGTZ_ErrorCode ret = CAEN_DGTZ_Success;
+  int handle = 0;
+
+  int id = 0;
+  while (ret == CAEN_DGTZ_Success) {
+    ret = CAEN_DGTZ_OpenDigitizer(CAEN_DGTZ_USB, id, 0, 0, &handle);
+    if (ret == 0) {
+      CAEN_DGTZ_BoardInfo_t bInfo;
+      CAEN_DGTZ_GetInfo(handle, &bInfo);
+      cout << bInfo.Channels << " channel, " << bInfo.ADC_NBits << " bit " << bInfo.ModelName
+           << " found with id " << id << " and handle " << handle << endl;
+      CAEN_DGTZ_CloseDigitizer(handle);
+    } else if (id == 0) {
+      cout << "no digitizers found." << endl;
+    }
+
+    ++id;
+  }
+}
