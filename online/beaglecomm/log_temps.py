a34a56d023a9cdd519b614f6ed3c098787605e32
frascati test beam daq computer <fienberg@uw.edu>
2016-03-08 13:22:19 +0100

    before doing flight simulator tests in lab

--- online/beaglecomm/log_temps.py
+++ online/beaglecomm/log_temps.py
@@ -11,13 +11,13 @@ def main():
         print 'wait time must be a number. you entered %s' % sys.argv[2]
         sys.exit(0)
     with open("temperaturedata/%s" % sys.argv[1], "w") as outf:
-        outf.write('date, time, left, middle, right, top\n')
+        outf.write('date, time, bottom, left, middle, right, top\n')
     while True:
         with open("temperaturedata/%s" % sys.argv[1], "a") as outf:
-            temps = [str(beagle.read_temp(j)) for j in xrange(1, 5)]
+            temps = [str(beagle.read_temp(j)) for j in xrange(5)]
             print temps
             now = datetime.now()
-            outf.write('%s, %s, %s, %s, %s, %s\n' % (('%02i/%02i/%02i' % (now.day, now.month, now.year), '%02i:%02i:%02i' % (now.hour, now.minute, now.second)) + tuple(temps)))
+            outf.write('%s, %s, %s, %s, %s, %s, %s\n' % (('%02i/%02i/%02i' % (now.day, now.month, now.year), '%02i:%02i:%02i' % (now.hour, now.minute, now.second)) + tuple(temps)))
         time.sleep(wait_time)
 
 if __name__ == "__main__":
